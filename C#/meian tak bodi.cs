7323d7417f18a489307f917e9b392ea6c8021f7c
alihj <49589500+alihj@users.noreply.github.com>
2023-04-29 18:13:10 +0330

    Add files via upload

---
+++ C#/meian tak bodi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[50];
+            Console.WriteLine("Please enter 50 numbers to calculate:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int meian = Calculate(array);
+            Console.WriteLine( meian);
+            Console.ReadLine();
+        }
+
+        static int Calculate(int[] arr)
+        {
+            Array.Sort(arr);
+            int Index = arr.Length / 2;
+            int z = 0;
+            if (arr.Length % 2 == 0)
+            {
+                z = (arr[Index - 1] + arr[Index]) / 2;
+            }
+            else
+            {
+                z = arr[Index];
+            }
+            return z;
+        }
+    }
+}
